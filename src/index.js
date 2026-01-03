e37a17533b2b2a035bde80f926ecd52c77fbd04e
daule1999 <dauleshwar.k@gmail.com>
2026-01-03 13:42:41 +0530

    Revert "Fix: Allow localhost CORS in production for testing"
    This reverts commit 4ebfc89a5770efacf865169680953335dc0cde5f.

--- src/index.js
+++ src/index.js
@@ -25,12 +25,9 @@ const { sanitizeInput } = require('./middleware');
 
 // Middleware
 app.use(cors({
-    origin: [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:3000',
-        process.env.FRONTEND_URL
-    ].filter(Boolean),
+    origin: process.env.NODE_ENV === 'production'
+        ? process.env.FRONTEND_URL
+        : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
     credentials: true
 }));
 app.use(express.json());
